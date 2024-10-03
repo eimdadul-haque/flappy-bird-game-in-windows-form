737728abde2e8e6708cfaa379be3b6a408e5045f
DESKTOP-375QGR3\Eimdadul Haque <eimdadulhaque@gmail.com>
2024-10-03 16:37:13 +0600

    bird, pipe and ground image is added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            Bird = new PictureBox();
+            PipeDown = new PictureBox();
+            PipeUp = new PictureBox();
+            Ground = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)Bird).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PipeDown).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PipeUp).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Ground).BeginInit();
+            SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimerEvent;
+            // 
+            // Bird
+            // 
+            Bird.Image = Properties.Resources.bird;
+            Bird.Location = new Point(103, 219);
+            Bird.Name = "Bird";
+            Bird.Size = new Size(60, 53);
+            Bird.SizeMode = PictureBoxSizeMode.StretchImage;
+            Bird.TabIndex = 0;
+            Bird.TabStop = false;
+            // 
+            // PipeDown
+            // 
+            PipeDown.Image = Properties.Resources.pipedown;
+            PipeDown.Location = new Point(364, -70);
+            PipeDown.Name = "PipeDown";
+            PipeDown.Size = new Size(100, 261);
+            PipeDown.SizeMode = PictureBoxSizeMode.StretchImage;
+            PipeDown.TabIndex = 1;
+            PipeDown.TabStop = false;
+            // 
+            // PipeUp
+            // 
+            PipeUp.Image = Properties.Resources.pipe;
+            PipeUp.Location = new Point(364, 327);
+            PipeUp.Name = "PipeUp";
+            PipeUp.Size = new Size(100, 282);
+            PipeUp.SizeMode = PictureBoxSizeMode.StretchImage;
+            PipeUp.TabIndex = 2;
+            PipeUp.TabStop = false;
+            // 
+            // Ground
+            // 
+            Ground.Image = Properties.Resources.ground;
+            Ground.Location = new Point(-19, 551);
+            Ground.Name = "Ground";
+            Ground.Size = new Size(707, 78);
+            Ground.SizeMode = PictureBoxSizeMode.StretchImage;
+            Ground.TabIndex = 3;
+            Ground.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Cyan;
+            ClientSize = new Size(653, 621);
+            Controls.Add(Ground);
+            Controls.Add(PipeUp);
+            Controls.Add(PipeDown);
+            Controls.Add(Bird);
+            Name = "Form1";
+            Text = "Flappy Bird";
+            KeyDown += GameKeyIsDown;
+            KeyUp += GameKeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)Bird).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PipeDown).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PipeUp).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Ground).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer GameTimer;
+        private PictureBox Bird;
+        private PictureBox PipeDown;
+        private PictureBox PipeUp;
+        private PictureBox Ground;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,29 @@ namespace flappy_bird_game_in_windows_form
 {
     public partial class Form1 : Form
     {
+
+        int pipeSpeed = 8;
+        int gravity = 5;
+        int score = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameKeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void GameKeyIsDown(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
